c4b72f47606fc961f9350b749ee82705d8009c93
Nicola <nicola.vianello@gmail.com>
2018-07-03 15:51:54 +0200

    update to core

--- eqtools/core.py
+++ eqtools/core.py
@@ -8,7 +8,7 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# eqtools is distributed in the hope that it will be useful,
+# eqtools is distributed in the hope that it will be usul,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
